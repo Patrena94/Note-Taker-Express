8c0b4ceccefba47db44a331c3ad8480d8e90e545
Vashti <patrena@sbcglobal.net>
2021-08-07 16:17:36 -0700

    steps 1 and 2 completed

--- Develop/server.js
+++ Develop/server.js
@@ -20,42 +20,33 @@ app.use(express.json());
 app.use(express.static('public'));
 
 // // GET methods
-// app.get('/', (req, res) => {
-//  res.send ("Welcome to Note Taker");
-// });
+app.get('/api/notes', (req, res) => {
+    res.send(notes);
+  });
 
-// app.get('/notes', (req, res) => {
-//     res.send(notes);
-//   });
-
-//   app.get('/api/notes/:id', (req,res)=> {
-//       res.send(notes[req.params.id]);
-//   });
-
-
-// // app.post('/api/notes', (req,res)=>{
-// //     res.send('notes')
-// // })
+  app.get('/api/notes/:id', (req,res)=> {
+      res.send(notes[req.params.id]);
+  });
 
 //Port Listener
 app.listen(3001,()=>{
     console.log(`API server now on port 3001!`);
 });
-//   // POST Method Route
-// app.post('/api/notes', (req,res) => {
-//     req.body.id = notes.length.toString();
-//     // If any data in req.body is incorrect, send error back
-//     // if(!validateNotes(req.body)) {
-//     //     res.status(400).send('note is not properly formatted.');
-//     // } else {
-//     //     const notes = createNewNotes(req.body, notes);
-//         // const createnewNote=req.body; notes.push(createnewNotes)
-//         // return console.log("added new note:"+newNote.title)
-//         // console.log(req.body);
-//         res.json(req.body.id, [notes]);
-//     // }
-// // }
-// });
+  // POST Method Route
+app.post('/api/notes', (req,res) => {
+    req.body.id = notes.length.toString();
+    // If any data in req.body is incorrect, send error back
+    if(!validateNotes(req.body)) {
+        res.status(400).send('note is not properly formatted.');
+    } else {
+        const notes = createNewNotes(req.body, notes);
+        const createnewNote=req.body; notes.push(createnewNotes)
+        // return console.log("added new note:"+newNote.title)
+        // console.log(req.body);
+        res.json('notes');
+    }
+// }
+});
 // // app.delete('/', (req, res)=> {
 // //     res.send('Delete request to homepag')
 // // })
