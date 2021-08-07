2a7ffe0136577f15e3e9450cbe3659bf508542d0
Vashti <patrena@sbcglobal.net>
2021-08-07 14:43:14 -0700

    changes review

--- Develop/server.js
+++ Develop/server.js
@@ -5,8 +5,8 @@ const express = require('express');
 const { Router } = require('express');
 
 const {notes} = require('./data/db.json');
-const apiRoutes = require('./routes/apiRoutes')
-const htmlRoutes = require('./routes/htmlRoutes');
+// const apiRoutes = require('./routes/apiRoutes');
+// const htmlRoutes = require('./routes/htmlRoutes');
 const { networkInterfaces } = require('os');
 const { query } = require('express');
 const { title } = require('process');
@@ -19,44 +19,44 @@ app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use(express.static('public'));
 
-// GET methods
-app.get('/', (req, res) => {
- res.send ("Welcome to Note Taker");
-});
+// // GET methods
+// app.get('/', (req, res) => {
+//  res.send ("Welcome to Note Taker");
+// });
 
-app.get('/notes', (req, res) => {
-    res.send(notes);
-  });
+// app.get('/notes', (req, res) => {
+//     res.send(notes);
+//   });
 
-  app.get('/api/notes/:id', (req,res)=> {
-      res.send(notes[req.params.id]);
-  });
+//   app.get('/api/notes/:id', (req,res)=> {
+//       res.send(notes[req.params.id]);
+//   });
 
 
-// app.post('/api/notes', (req,res)=>{
-//     res.send('notes')
-// })
+// // app.post('/api/notes', (req,res)=>{
+// //     res.send('notes')
+// // })
 
 //Port Listener
 app.listen(3001,()=>{
     console.log(`API server now on port 3001!`);
 });
-  // POST Method Route
-app.post('/api/notes', (req,res) => {
-    req.body.id = notes.length.toString();
-    // If any data in req.body is incorrect, send error back
-    // if(!validateNotes(req.body)) {
-    //     res.status(400).send('note is not properly formatted.');
-    // } else {
-    //     const notes = createNewNotes(req.body, notes);
-        // const createnewNote=req.body; notes.push(createnewNotes)
-        // return console.log("added new note:"+newNote.title)
-        // console.log(req.body);
-        res.json(req.body.id, [notes]);
-    // }
-// }
-});
-// app.delete('/', (req, res)=> {
-//     res.send('Delete request to homepag')
-// })
-// // app.delete(pathm callback[, callback])
+//   // POST Method Route
+// app.post('/api/notes', (req,res) => {
+//     req.body.id = notes.length.toString();
+//     // If any data in req.body is incorrect, send error back
+//     // if(!validateNotes(req.body)) {
+//     //     res.status(400).send('note is not properly formatted.');
+//     // } else {
+//     //     const notes = createNewNotes(req.body, notes);
+//         // const createnewNote=req.body; notes.push(createnewNotes)
+//         // return console.log("added new note:"+newNote.title)
+//         // console.log(req.body);
+//         res.json(req.body.id, [notes]);
+//     // }
+// // }
+// });
+// // app.delete('/', (req, res)=> {
+// //     res.send('Delete request to homepag')
+// // })
+// // // app.delete(pathm callback[, callback])
