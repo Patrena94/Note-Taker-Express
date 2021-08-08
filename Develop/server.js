f6a2052bd1d4f8cf607cb9d58d26aff744116186
Vashti <patrena@sbcglobal.net>
2021-08-07 17:36:18 -0700

    added validation

--- Develop/server.js
+++ Develop/server.js
@@ -5,8 +5,8 @@ const express = require('express');
 const { Router } = require('express');
 
 const {notes} = require('./data/db.json');
-// const apiRoutes = require('./routes/apiRoutes');
-// const htmlRoutes = require('./routes/htmlRoutes');
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
 const { networkInterfaces } = require('os');
 const { query } = require('express');
 const { title } = require('process');
@@ -19,33 +19,83 @@ app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use(express.static('public'));
 
+// functions
+function createNewNote(body, notesArray){
+    const note = body;
+    notesArray.push(note);
+    fs.writeFileSync(path.join(__dirname,'.data/notes.json'),
+    JSON.stringify({notes: notesArray}, null, 2)
+    );
+    return note;
+}
+function validateNote (note){
+    if(!note.title || typeof note.title !== 'string'){
+        return false;
+    }
+    if(!note.text || typeof note.text !=='string'){
+        return false;
+    }
+    return true;
+    }
+function filterbyQuery(query, notesArray) {
+    let title=[];
+    if(query.title){
+        if(typeof query.title ==='string'){
+         } else {
+                title=query.title;
+            }
+            title.forEach(title =>{
+                filteredResults = filteredResults. filter(notes=>
+                    notes.title.indexof(title)!== -1);
+                });
+            }
+        return filteredResults;
+    }
+    
+function findById(id, notesArray){
+    let result = notesArray.filter(animal => notes.id===id)[0];
+    return result;
+}
+
 // // GET methods
 app.get('/api/notes', (req, res) => {
     res.send(notes);
   });
 
   app.get('/api/notes/:id', (req,res)=> {
-      res.send(notes[req.params.id]);
-  });
+      let result=findById(req.params.id, notes);
+      if(result){
+          res.json(result);
+      } else {res.send(44);
+    }    
+      });
+    //   res.send(notes[req.params.id]);
+
 
 //Port Listener
 app.listen(3001,()=>{
     console.log(`API server now on port 3001!`);
 });
   // POST Method Route
+  
 app.post('/api/notes', (req,res) => {
+//     let newNote=req.body;
+//     notes.push(newNote);
+//     // updateDb();
+//     return console.log("New note:"+newNote.title);
+// });
     req.body.id = notes.length.toString();
-    // If any data in req.body is incorrect, send error back
-    if(!validateNotes(req.body)) {
+    // // If any data in req.body is incorrect, send error back
+    if(!validateNote (req.body)) {
         res.status(400).send('note is not properly formatted.');
     } else {
-        const notes = createNewNotes(req.body, notes);
-        const createnewNote=req.body; notes.push(createnewNotes)
+        const note = createNewNote(req.body, notes);
+    //     const createnewNote=req.body; notes.push(createnewNotes)
         // return console.log("added new note:"+newNote.title)
         // console.log(req.body);
-        res.json('notes');
+        res.json('note');
     }
-// }
+// // }
 });
 // // app.delete('/', (req, res)=> {
 // //     res.send('Delete request to homepag')
